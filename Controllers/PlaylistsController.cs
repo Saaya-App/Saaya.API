dd246aa4eaa7aed1358b2dd1929382c92e0c4fa6
句和一 <alice@aisys.dev>
2023-08-03 05:32:44 -0600

    Endpoints for adding songs/playlists

--- Controllers/PlaylistsController.cs
+++ Controllers/PlaylistsController.cs
@@ -2,6 +2,7 @@
 using Saaya.API.Db;
 using Saaya.API.Db.Extensions;
 using Saaya.API.Db.Models;
+using Saaya.API.Services;
 
 namespace Saaya.API.Controllers
 {
@@ -9,12 +10,15 @@ namespace Saaya.API.Controllers
     public class PlaylistsController : Controller
     {
         private readonly ILogger<PlaylistsController> _logger;
-        private readonly ApiContext _db;
 
-        public PlaylistsController(ILogger<PlaylistsController> logger, ApiContext db)
+        private readonly ApiContext _db;
+        private readonly LibraryService _library;
+
+        public PlaylistsController(ILogger<PlaylistsController> logger, ApiContext db, LibraryService library)
         {
             _logger = logger;
             _db = db;
+            _library = library;
         }
 
         [HttpGet]
@@ -28,6 +32,21 @@ namespace Saaya.API.Controllers
                 return BadRequest(new List<Playlist>());
 
             return Ok(_db.Users.GetPlaylists(AuthToken) ?? new List<Playlist>());
+        }
+
+        [HttpPost("{playlist}")]
+        public async Task<IActionResult> AddPlaylist(string playlist)
+        {
+            string AuthToken = HttpContext.Request.Headers["Authorization"].ToString().Split(" ")[1];
+            if (string.IsNullOrEmpty(AuthToken))
+                return BadRequest();
+
+            if (!_db.Users.UserExists(AuthToken))
+                return BadRequest();
+
+            await _library.DownloadPlaylist(playlist, _db.Users.Where(x => x.Token == AuthToken).FirstOrDefault());
+
+            return Ok();
         }
     }
 }
--- Controllers/SongsController.cs
+++ Controllers/SongsController.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Saaya.API.Db;
 using Saaya.API.Db.Extensions;
 using Saaya.API.Db.Models;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+using Saaya.API.Services;
 
 namespace Saaya.API.Controllers
 {
@@ -12,12 +10,15 @@ namespace Saaya.API.Controllers
     public class SongsController : ControllerBase
     {
         private readonly ILogger<SongsController> _logger;
+
         private readonly ApiContext _db;
+        private readonly LibraryService _library;
         
-        public SongsController(ILogger<SongsController> logger, ApiContext db)
+        public SongsController(ILogger<SongsController> logger, ApiContext db, LibraryService library)
         {
             _logger = logger;
             _db = db;
+            _library = library;
         }
 
         [HttpGet]
@@ -44,6 +45,21 @@ namespace Saaya.API.Controllers
                 return BadRequest(new List<Song>());
 
             return Ok(_db.Users.GetPlaylistSongs(AuthToken, playlist) ?? new List<Song>());
+        }
+
+        [HttpPost("{song}")]
+        public async Task<IActionResult> AddSongForUser(string song)
+        {
+            string AuthToken = HttpContext.Request.Headers["Authorization"].ToString().Split(" ")[1];
+            if (string.IsNullOrEmpty(AuthToken))
+                return BadRequest();
+
+            if (!_db.Users.UserExists(AuthToken))
+                return BadRequest();
+            
+            await _library.DownloadSong(song, _db.Users.Where(x => x.Token == AuthToken).FirstOrDefault());
+
+            return Ok();
         }
     }
 }
